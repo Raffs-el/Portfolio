3dbd1e5b7f6c8ed05652f10c119a75567095e749
Rafael <rafaell_mirandaa@outlook.com>
2023-02-24 10:19:53 -0300

    Portfólio v1.0

--- home.js
+++ home.js
@@ -117,6 +117,22 @@ const observer3 = new IntersectionObserver ((entries) => {
   
 
 
+  const observer4 = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('mostrar4');
+        } else {
+            entry.target.classList.remove('mostrar4');
+        }
+  
+    });
+  });
+  
+  const hiddenElements4 = document.querySelectorAll('.escondido4');
+  hiddenElements4.forEach((el) => observer4.observe(el));
+
+
 
 
 
